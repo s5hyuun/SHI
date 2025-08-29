ba558d680c5fa56da9a45f7ae79b2a2e2107e347
suhyun <wendy132@naver.com>
2025-08-29 09:58:23 +0900

    useCallback

---
+++ 03_react-memo-callback/src/App.jsx
@@ -0,0 +1,16 @@
+// import Parent from "./Parent1_memo";
+// import Parent from "./Parent1_no_memo";
+
+// import Parent from "./Parent2_callback";
+// import Parent from "./Parent2_no_callback";
+
+import Parent from "./Parent3_memo_callback";
+// import Parent from "./Parent3_no_memo_callback";
+
+function App() {
+  return (
+    <Parent />
+  );
+}
+
+export default App;
