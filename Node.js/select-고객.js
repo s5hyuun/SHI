5798dc9973c5584fa7d415c1f2bc4798a7b195de
s5hyuun <wendy132@naver.com>
2025-09-11 13:50:35 +0900

    connect SQL

--- Node.js/select-고객.js
+++ Node.js/select-고객.js
@@ -8,24 +8,26 @@ const pool = mysql.createPool({  // ① 데이터베이스 연결
 })
 
 // 조회할 떄 조건이 없으므로 함수의 미개변수도 없다 
+// 비동기 함수
 const getUsers = async () => {
   try {
     const [rows] = 
         await pool.query( 
             'SELECT 고객아이디, 이름, 나이, 등급, 직업, 적립금 FROM 고객'
         );  
-
-    console.log(rows);  
+    return rows;
   } catch (e) {
     console.error('조회 실패:', e.message);
   }
 };
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     // 고객명, 등급, 나이, ...
     // url.parse(req.rul, true);
-    getUsers();
-    res.end();
+    const rows = await getUsers();
+    console.log(rows);
+    res.setHeader("content-type", "application/json; charset=utf-8");
+    res.end(JSON.stringify(rows)); // 문자 string 
 });
 
 server.listen(3000, () => {
