bbb0913c413f7eb19561018bb60e74c8e0e8e4d6
s5hyuun <wendy132@naver.com>
2025-09-15 10:56:58 +0900

    greet

--- Node.js/03/app-greet.js
+++ Node.js/03/app-greet.js
@@ -3,9 +3,6 @@ import greetRouter from './greet.js';
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.use('/greet', greetRouter);
 
 app.use((req, res) => {
