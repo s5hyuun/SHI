40ecd755ac460e6aaf2b37c1d435e957807b840b
s5hyuun <wendy132@naver.com>
2025-09-15 14:52:25 +0900

    download

--- Node.js/04/public/res-express-json.js
+++ Node.js/04/public/res-express-json.js
@@ -8,12 +8,25 @@ app.get("/down1", (req, res) => {
     // res.download('../다운로드/x.txt');
     // 현재 경로 확인하기
     console.log( process.cwd() );
-    console.log( path.resolve() ); // 프로젝트 경로
+    console.log( path.resolve() ); // node를 실행한 위치 
     console.log( import.meta.dirname ); // 실행되는 파일의 경로
 
     // res.download( path.xresolve() + "04/public/example,txt");
 
     res.download( path.join(path.resolve(), '04', 'public', 'example.txt') );
+
+    // 1
+    app.get("/donw1", (req, res) => {
+        res.sownload (path.join(path.resolve(), '04', 'public',  'example.txt'));
+    });
+
+    // 2
+    app.get("/donw2", (req, res) => {
+        res.sownload (path.join(path.resolve(), '04', 'public',  'example.txt'));
+    });
+
+    // 3
+    
 
     // res.send("1");
 })
