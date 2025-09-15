b064d095709df4813892ce1b40f322d559fcd7bf
s5hyuun <wendy132@naver.com>
2025-09-15 15:16:10 +0900

    express-image

--- Node.js/04/image-header.js
+++ Node.js/04/image-header.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.get("/image", (req, res) => {
     res.sendFile( path.join(path.resolve(), "04/public/node.png"));
+    
+// app.get('/image', (req, res) => {
+//     const filePath = path.join(import.meta.dirname, '../public', 'node.png');
+// })  // import.meta.dirname; >> 현재 코드가 실행되고 있는 위치
+    
 });
 
 app.get("/download", (req, res) => {
