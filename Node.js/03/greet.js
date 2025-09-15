9aedf36b46ec491b01880a2cd4506b61fc89e54b
s5hyuun <wendy132@naver.com>
2025-09-15 11:04:47 +0900

    greet

--- Node.js/03/greet.js
+++ Node.js/03/greet.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 const router = Router();
 
+
+router.get('/', (req, res) => {
+    res.status(404).send(`페이지 없음`);
+});
+
 router.get('/:name', (req, res) => {
     const name = req.params.name;
-    if (!name) {
-        return res.status(404).send('이름이 없습니다');
-    }
     res.send(`<h2> 안녕하세요, ${name}님! </h2>`);
 });
 
